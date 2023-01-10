57503b064c6d7638f1d902dd03ec0546419b5bf2
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-01-11 01:07:33 +0900

    #1 CHAT WITH WEBSOCKETS ~ 1-8 닉네임 설정 후 채팅하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,42 @@
+const messageList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
+
 // const socket = new WebSocket('ws://localhost:3000');
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener('open', () => {
   console.log('Connected to Browser ✅');
 });
 
 socket.addEventListener('message', (message) => {
-  console.log('New message: ', message.data, ' from the Server');
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener('close', () => {
   console.log('Disconnected to Browser ❌');
 });
 
-setTimeout(() => {
-  socket.send('hello from the browser');
-}, 10000);
+function handleSubmit(event) {
+  event.preventDefault();  // 실제로 form이 submit하지 않게 막아준다.
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();  // 실제로 form이 submit하지 않게 막아준다.
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -16,7 +16,11 @@ const handleListen = () => console.log(`Listening on http://localhost:3000 `);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on('connection', (socket) => {
+  sockets.push(socket);
+  socket['nickname'] = 'Anon';
 
   console.log('Connected to Server ✅');
 
@@ -24,11 +28,17 @@ wss.on('connection', (socket) => {
     console.log('Disconnected from the Browser ❌');
   });
 
-  socket.on('message', (message) => {
-    console.log(message.toString('utf8'));
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) => {
+          aSocket.send(`${socket.nickname}: ${message.payload}`);
+        });
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
   });
-
-  socket.send('hello!?');
 });
 
 server.listen(3000, handleListen);
