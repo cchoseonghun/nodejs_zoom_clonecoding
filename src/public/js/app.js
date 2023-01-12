c45f55d60ef99d41e15a119c4c903a3f7e95496b
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-01-12 09:54:23 +0900

    #2 SOCKETIO ~ 2-6 특정 방에 접속 후 메시지 주고받기 기능 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,19 +2,60 @@ const socket = io();
 // io function이 알아서 socket.io를 실행하고 있는 서버를 찾는다.
 
 const welcome = document.querySelector('#welcome');
-const form = document.querySelector('form');
+const welcomeForm = welcome.querySelector('form');
+const room = document.querySelector('#room');
+
+room.hidden = true;
+let roomName;
+
+function addMessage(message) {
+  const ul = room.querySelector('ul');
+  const li = document.createElement('li');
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+welcomeForm.addEventListener('submit', handleRoomSubmit);
 
 function handleRoomSubmit(event) {
   event.preventDefault();  // 작동해야할 기능을 막아줌 ex) submit
-  const input = form.querySelector('input');
-  socket.emit('enter_room', { payload: input.value }, () => {
-    console.log('server is done');
-  });
-  // socket.emit에서 
-  // 첫 번째 argument -> event 이름
-  // 두 번째 argument -> 보내고 싶은 payload
-  // 세 번째 argument -> 서버에서 호출하는 function *중요*
+
+  const input = welcomeForm.querySelector('input');
+  socket.emit('enter_room', input.value, showRoom);
+  // 첫 번째: event name
+  // 두 번째 ~: 전달할 값 마음대로 가능
+  // 마지막: back-end에서 front-end에 실행시킬 function 
+  roomName = input.value;
   input.value = '';
 }
 
-form.addEventListener('submit', handleRoomSubmit);
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName}`;
+
+  const roomForm = room.querySelector('form');
+  roomForm.addEventListener('submit', handleMessageSubmit);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+
+  const input = room.querySelector('input');
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`You: ${input.value}`);
+    input.value = '';
+  });
+}
+
+socket.on('welcome', () => {
+  addMessage('someone joined');
+})
+
+socket.on('bye', () => {
+  addMessage('someone left ㅠㅠ');
+})
+
+socket.on('new_message', addMessage);
--- src/server.js
+++ src/server.js
@@ -19,14 +19,35 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-  socket.on('enter_room', (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done(); 
-    }, 10000);
-    // 서버는 back-end에서 done function을 호출하지만 
-    // done function은 front-end에서 실행된다.
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
   });
+  socket.on('enter_room', (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit('welcome');  // 자신을 제외한 방안의 모두에게 전송
+    // socket.to(rooName).emit()
+    // console.log(socket.id);  // 유저 id이자 유저의 id로 자동생성되는 room
+    // console.log(socket.rooms);  // rooms 목록
+    // socket.join(roomName);  // roomName으로 입장
+    // console.log(socket.rooms);
+    // setTimeout(() => {
+    //   done('hello from back-end');  
+    //   // 마지막으로 전달한 함수를 front-end에서 실행시킨다.
+    //   // 헷갈릴 수 있지만 애초에 front-end에서 전달한 함수를
+    //   // back-end에서 실행시킨다는 것은 보안상으로 매우매우 위험한 일이므로 
+    //   // 일어날 수 없는 일로 생각하면 쉬움
+    // }, 5000);
+  });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit('bye');
+    });
+  })
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', msg);
+    done();
+  })
 });
 
 
