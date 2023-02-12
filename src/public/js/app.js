8c9aac8aa2fc6160e8df0f693528193e6493fc1d
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-02-13 04:34:51 +0900

    #3 VIDEO CALL ~ 3-4 Rooms 방 생성, 참가 기능 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,14 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById('call');
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
 async function getCameras() {
   try {
@@ -56,7 +60,7 @@ async function getMedia(deviceId) {
   }
 }
 
-getMedia();
+// getMedia();
 
 function handleMuteClick() {
   myStream
@@ -91,3 +95,31 @@ async function handleCameraChange() {
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+
+const welcome = document.getElementById('welcome');
+const welcomeForm = welcome.querySelector('form');
+
+function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  getMedia();
+}
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector('input');
+  // console.log(input.value);
+  socket.emit('join_room', input.value, startMedia);
+  roomName = input.value;
+  input.value = '';
+}
+
+welcomeForm.addEventListener('submit', handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on('welcome', () => {
+  console.log('someone joined');
+})
--- src/server.js
+++ src/server.js
@@ -1,29 +1,38 @@
-import http from 'http';
-// import SocketIO from 'socket.io';
-import { Server } from 'socket.io';
-import { instrument } from '@socket.io/admin-ui';
-import express from 'express';
+import http from "http";
+import SocketIO from "socket.io";
+// import { Server } from 'socket.io';
+// import { instrument } from '@socket.io/admin-ui';
+import express from "express";
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/'));
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-// const wsServer = SocketIO(httpServer);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ['https://admin.socket.io'], 
-    credentials: true, 
-  }, 
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit('welcome');
+  });
 });
 
-instrument(wsServer, {
-  auth: false
-})
+// const wsServer = new Server(httpServer, {
+//   cors: {
+//     origin: ['https://admin.socket.io'],
+//     credentials: true,
+//   },
+// });
+
+// instrument(wsServer, {
+//   auth: false
+// })
 
 const handleListen = () => console.log(`Listening on http://localhost:3000 `);
 httpServer.listen(3000, handleListen);
