00554acae4d3072f674c0bc3f6eb4497d2ed69cd
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-02-12 18:56:21 +0900

    #3 VIDEO CALL 세팅 - 모든 채팅 관련 코드 삭제

--- src/server.js
+++ src/server.js
@@ -25,58 +25,5 @@ instrument(wsServer, {
   auth: false
 })
 
-function publicRooms() {
-  const { sids, rooms } = wsServer.sockets.adapter;
-  // sids: private rooms
-  // rooms: public rooms
-  const publicRooms = [];
-
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  })
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on('connection', (socket) => {
-  socket['nickname'] = 'Anonymous';
-
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-
-  socket.on('enter_room', (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit('room_change', publicRooms());
-  });
-
-  // socket이 방을 떠나기 바로 직전에 실행하는 disconnecting event
-  socket.on('disconnecting', () => {
-    socket.rooms.forEach((room) => {
-      socket.to(room).emit('bye', socket.nickname, countRoom(room) -1);
-    });
-  });
-
-  socket.on('disconnect', () => {
-    wsServer.sockets.emit('room_change', publicRooms());
-  });
-
-  socket.on('new_message', (msg, roomName, done) => {
-    socket.to(roomName).emit('new_message', `${socket.nickname}: ${msg}`);
-    done();
-  });
-
-  socket.on('nickname', (nickname) => {
-    socket['nickname'] = nickname;
-  });
-});
-
 const handleListen = () => console.log(`Listening on http://localhost:3000 `);
 httpServer.listen(3000, handleListen);
